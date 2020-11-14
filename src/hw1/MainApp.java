902696b6dbd57ff7faa26e4f40ca25691484c89d
FF <5kla@mail.ru>
2020-11-15 00:24:59 +0500

    hw1

--- src/hw1/MainApp.java
+++ src/hw1/MainApp.java
@@ -2,6 +2,10 @@ package hw1;
 
 public class MainApp {
     public static void main(String[] args) {
+        System.out.println("Hello github!");
+    }
 
+    public static void work1(){
+        System.out.println("My first method!");
     }
 }
